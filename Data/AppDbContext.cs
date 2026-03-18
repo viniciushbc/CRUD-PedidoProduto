02c1bba5689e5b2adc6f9419fb85e4695e81e44b
viniciushbc <vinehbc@gmail.com>
2026-03-18 17:40:48 -0300

    feat: add AppDbContext and relationship configuration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using CrudPedidoProduto.Models;
+
+namespace CrudPedidoProduto.Data;
+
+
+public class AppDbContext : DbContext {
+
+    // DbSet é o modulo do EF Core que representa uma tabela
+    // Criando tabelas onde cada linha corresponde a um Objeto
+    public DbSet<Pedido> Pedidos {get; set;};
+    public DbSet<Produto> Produtos {get;set;};
+    public DbSet<PedidoProduto> PedidosProdutos {get;set;};
+
+    // Recebe a configuração de conexão com o banco e repassa pra classe pai do EF Core (DbContext)
+    public AppDbContext(DbContextOptions<AppDbContext> options)  : base(options) {
+    }
+
+
+    // Configurações relacionais do banco
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+        // Criando uma chave composta pra tabela PedidoProduto
+        modelBuilder.Entity<PedidoProduto>()
+            .HasKey(pp => new {pp.PedidoId, pp.ProdutoId});
+        
+        // PEDIDO - Cada pedido tem vários produtos
+        // Estou conectando isso na tabela PedidoProduto através da chave estrangeira PedidoId
+        modelBuilder.Entity<PedidoProduto>()
+            .HasOne(pp => pp.Pedido)
+            .WithMany(p => p.PedidosProdutos)
+            .HasForeignKey(pp => pp.PedidoId);
+
+        // PRODUTO - Cada produto pertence à vários pedidos
+        // Estou conectando isso na tabela PedidoProduto através da chave estrangeira ProdutoId
+        modelBuilder.Entity<PedidoProduto>()
+            .HasOne(pp => pp.Produto)
+            .WithMany(p => p.PedidosProdutos)
+            .HasForeignKey(pp => pp.ProdutoId);
+    }
+}
+
