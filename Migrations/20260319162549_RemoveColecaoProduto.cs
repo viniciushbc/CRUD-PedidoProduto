510ecf1f55e5bd679183c5ecc39b7288bef1be93
viniciushbc <vinehbc@gmail.com>
2026-03-19 13:28:33 -0300

    refactor: Rearrange relationships

---
+++ Migrations/20260319162549_RemoveColecaoProduto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUD_PedidoProduto.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveColecaoProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
