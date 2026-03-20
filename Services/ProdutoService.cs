4441e3ce19536a5595d2934a16b945c5cb234507
viniciushbc <vinehbc@gmail.com>
2026-03-20 00:51:00 -0300

    feat: add initial Services files

---
+++ Services/ProdutoService.cs
@@ -0,0 +1,21 @@
+namespace CrudPedidoProduto.Services {
+
+using Microsoft.EntityFrameworkCore;
+using CrudPedidoProduto.Data;
+using CrudPedidoProduto.DTOs;
+using CrudPedidoProduto.Models;
+
+public class ProdutoService {
+
+
+    private AppDbContext AcessoAoDB;
+
+    public ProdutoService(AppDbContext Contexto){
+        AcessoAoDB = Contexto;
+    }
+
+
+    public
+
+}
+}
