540cd2d05e42b2d9fb0dd4b3be7461de773800e8
viniciushbc <vinehbc@gmail.com>
2026-03-20 21:58:54 -0300

    fix: correct class reference name

--- DTOs/RespostaPedidoDto.cs
+++ DTOs/RespostaPedidoDto.cs
@@ -3,9 +3,9 @@ namespace CrudPedidoProduto.DTOs {
     // Response de Pedido
 
     public class RespostaPedidoDto {
-        public int Id {get;set;};
+        public int Id {get;set;}
         public int NumeroPedido {get;set;}
-        public List<RespostaPedidoItem> Itens {get;set;} = new List<RespostaPedidoItem>();
+        public List<RespostaPedidoItemDto> Itens {get;set;} = new List<RespostaPedidoItemDto>();
         public decimal ValorTotal {get;set;}
     }
 }
