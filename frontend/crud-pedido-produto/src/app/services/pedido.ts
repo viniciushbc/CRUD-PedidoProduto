2756f11cdca633199464c51673e2704f12325440
viniciushbc <vinehbc@gmail.com>
2026-03-22 19:42:00 -0300

    feat: add pedido service (setup and http methods)

---
+++ frontend/crud-pedido-produto/src/app/services/pedido.ts
@@ -0,0 +1,39 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CriarPedidoDto } from '../models/pedido.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+
+// Configurações iniciais (Quem vai fazer as requisições e a rota base)
+export class PedidoService {
+  private http = inject(HttpClient);
+  private apiEndpoint = "http://localhost:5260/api/pedidos";
+
+  // Verbos HTTP
+  getPedidos(){
+    return this.http.get(this.apiEndpoint);
+  }
+
+  getPedidoPorId(id: number){
+    return this.http.get(this.apiEndpoint + "/" + id);
+  }
+
+  getPedidoPorNumeroPedido(numPedido: number){
+    return this.http.get(this.apiEndpoint + "/" + numPedido);
+  }
+
+  postPedido(pedido: CriarPedidoDto){
+    return this.http.post(this.apiEndpoint, pedido);
+  }
+
+  putPedido(pedido: CriarPedidoDto, id: number){
+    return this.http.put(this.apiEndpoint + "/" + id, pedido);
+  }
+
+  deletePedido(id: number){
+    return this.http.delete(this.apiEndpoint + "/" + id);
+  }
+}
