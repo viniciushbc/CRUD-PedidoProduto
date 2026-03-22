a9c6af840c530eb1ebf3c7d54726510000ede124
viniciushbc <vinehbc@gmail.com>
2026-03-22 19:44:04 -0300

    feat: add produto service (setup and http methods)

--- frontend/crud-pedido-produto/src/app/services/produto.ts
+++ frontend/crud-pedido-produto/src/app/services/produto.ts
@@ -1,6 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CriarProdutoDto } from '../models/produto.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Produto {}
+
+// Configurações iniciasi (Quem vai fazer as requisições e a rota base)
+export class ProdutoService {
+  private http = inject(HttpClient); 
+  private apiEndpoint = "http://localhost:5260/api/produtos";
+
+
+  // Verbos HTTP
+  getProdutos(){
+    return this.http.get(this.apiEndpoint);
+  }
+
+  getProdutoPorId(id: number) {
+    return this.http.get(this.apiEndpoint + "/" + id);
+  }
+
+  postProduto(produto: CriarProdutoDto){
+    return this.http.post(this.apiEndpoint, produto);
+  }
+
+  putProduto(produto: CriarProdutoDto, id: number){
+    return this.http.put(this.apiEndpoint + "/" + id, produto);
+  }
+
+  deleteProduto(id: number) {
+    return this.http.delete(this.apiEndpoint + "/" + id);
+  }
+}
