45ed1195c98cd4689d6751cf8823b77fad831deb
viniciushbc <vinehbc@gmail.com>
2026-03-23 14:34:17 -0300

    feat: configure routes

--- frontend/crud-pedido-produto/src/app/app.routes.ts
+++ frontend/crud-pedido-produto/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { GerenciarProduto } from './pages/produto/gerenciar/gerenciar';
+import { EditarProduto } from './pages/produto/editar/editar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Produto
+    {path: 'produtos', component: GerenciarProduto }, // Página de produtos
+    {path: 'produtos/editar', component: EditarProduto}, // Formulario de criacao/edicao de produto
+    {path: 'produtos/editar/:id', component: EditarProduto} // Formulario de criacao/edicao de produto
+    
+];
