977cc446905694c9c0596689a62d58fab22dde25
viniciushbc <vinehbc@gmail.com>
2026-03-23 17:49:50 -0300

    feat: add pedido route

--- frontend/crud-pedido-produto/src/app/app.routes.ts
+++ frontend/crud-pedido-produto/src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { GerenciarProduto } from './pages/produto/gerenciar/gerenciar.component';
 import { EditarProduto } from './pages/produto/editar/editar.component';
+import { GerenciarPedido } from './pages/pedido/gerenciar/gerenciar.component';
+
 
 export const routes: Routes = [
     // Produto
     {path: 'produtos', component: GerenciarProduto }, // Página de produtos
     {path: 'produtos/editar', component: EditarProduto}, // Formulario de criacao/edicao de produto
-    {path: 'produtos/editar/:id', component: EditarProduto} // Formulario de criacao/edicao de produto
+    {path: 'produtos/editar/:id', component: EditarProduto}, // Formulario de criacao/edicao de produto
     
+    // Pedido
+    {path: 'pedidos', component: GerenciarPedido}, // Pagina de pedidos
+    //{path: 'pedidos/editar',}
+
 ];
