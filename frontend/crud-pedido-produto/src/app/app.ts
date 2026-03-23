c4614635f6cd8cd5aeecf103168498c6cce6fead
viniciushbc <vinehbc@gmail.com>
2026-03-23 00:59:19 -0300

    test: render products table

--- frontend/crud-pedido-produto/src/app/app.ts
+++ frontend/crud-pedido-produto/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Gerenciar } from './pages/produto/gerenciar/gerenciar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Gerenciar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
