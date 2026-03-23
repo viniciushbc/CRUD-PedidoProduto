87efee98ac987a880c7fb61e45f0cdebc8faa3f2
viniciushbc <vinehbc@gmail.com>
2026-03-23 14:36:08 -0300

    feat: add view/delete/edit funcions and configure routes

--- frontend/crud-pedido-produto/src/app/pages/produto/gerenciar/gerenciar.ts
+++ frontend/crud-pedido-produto/src/app/pages/produto/gerenciar/gerenciar.ts
@@ -2,7 +2,9 @@ import { Component, inject, ChangeDetectorRef } from '@angular/core';
 import { ProdutoService } from '../../../services/produto';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
 
+// TELA(COMPONENTE) DE VISUALIZAR/EDITAR/REMOVER PRODUTOS
 
 @Component({
   selector: 'app-gerenciar',
@@ -12,18 +14,43 @@ import { ButtonModule } from 'primeng/button';
 })
 
 
-export class Gerenciar {
+export class GerenciarProduto {
   private produtoService = inject(ProdutoService);
   private changeDetector = inject(ChangeDetectorRef);
+  private router = inject(Router);
 
   produtos: any[] = [];
 
   ngOnInit() {
     // subscribe agenda a minha requisição pra puxar todos os produtos
     // ao inves de usar o subscribe + detectChanges, daria pra usar signal
+    this.visualizarProdutos();
+    }
+
+  // GET da lista atualizada
+  visualizarProdutos(){
     this.produtoService.getProdutos().subscribe(data => {
       this.produtos = data;
       this.changeDetector.detectChanges();
+    });
+  }
+
+  // DELETAR PRODUTO
+  deletar(id: number){
+    this.produtoService.deleteProduto(id).subscribe( () => {
+
+      // Renderiza os produtos atualizados pós DELETE
+      this.visualizarProdutos();
     })
+  }
+
+  editar(id?: number){
+    console.log("Clicou");
+
+    if(id == null || id == undefined){
+      this.router.navigate(['/produtos/editar'])
+    } else {
+      this.router.navigate(['/produtos/editar', id]);
+    }
   }
 }
