c34a42465b20f3a46c32f89718ed0306119e1bbd
viniciushbc <vinehbc@gmail.com>
2026-03-24 12:22:11 -0300

    feat: add Pedido button to navigate between components

--- frontend/crud-pedido-produto/src/app/pages/pedido/gerenciar/gerenciar.component.ts
+++ frontend/crud-pedido-produto/src/app/pages/pedido/gerenciar/gerenciar.component.ts
@@ -38,6 +38,10 @@ export class GerenciarPedido {
     })
   }
 
+  acessarProdutos(){
+    this.router.navigate(['/produtos'])
+  }
+
 
   // Detalhes, produtos de um pedido
   detalhes(id: number){
